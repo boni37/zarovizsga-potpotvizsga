53459d72f201a565c934e79a096c53301396b849
boni37 <73605283+boni37@users.noreply.github.com>
2021-04-14 12:00:22 +0200

    HouseCup

--- zarovizsga-potpotvizsga/src/main/java/hu/nive/ujratervezes/zarovizsga/housecup/HouseCup.java
+++ zarovizsga-potpotvizsga/src/main/java/hu/nive/ujratervezes/zarovizsga/housecup/HouseCup.java
@@ -1,5 +1,28 @@
 package hu.nive.ujratervezes.zarovizsga.housecup;
 
+import org.mariadb.jdbc.MariaDbDataSource;
+
+import java.sql.*;
+
 public class HouseCup {
 
+    private final MariaDbDataSource dataSource;
+
+    public HouseCup(MariaDbDataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    public int getPointsOfHouse(String house) throws SQLException {
+        int result = 0;
+        try (Connection connection = dataSource.getConnection()) {
+            String selectPoints = "select sum(points_earned) as Points from house_points where house_name=?";
+            PreparedStatement stmt = connection.prepareStatement(selectPoints);
+            stmt.setString(1, house);
+            ResultSet rs = stmt.executeQuery();
+            if (rs.next()) {
+                result = rs.getInt("Points");
+            }
+        }
+        return result;
+    }
 }
